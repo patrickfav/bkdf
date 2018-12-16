3e339f6672ff0d573686b9dabe23b1b9ddc48ed1
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2018-12-17 00:05:55 +0100

    Add multi-kdf feature

--- src/main/java/at/favre/lib/crypto/bkdf/KeyDerivationFunction.java
+++ src/main/java/at/favre/lib/crypto/bkdf/KeyDerivationFunction.java
@@ -5,6 +5,7 @@ import at.favre.lib.crypto.HKDF;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 /**
  * Key Derivation protocol of BKDF. Used for derived high entropy secret keys from user passwords with a given cost factor.
@@ -68,9 +69,32 @@ public interface KeyDerivationFunction {
     byte[] derive(byte[] salt, byte[] ikm, int costFactor, byte[] infoParam, int outLengthByte);
 
     /**
+     * Derive multiple high entropy key material from given salt, user password.
+     * Use this to more efficiently calculate multiple output key materials with a single call. This will only
+     * calculate the expensive key stretching function only once for all outputs.
+     * <p>
+     * The output key material can be used as secret key like that:
+     *
+     * <pre>
+     *     List<byte[]> okmList = kdf.derive(...);
+     *     SecretKey secretKey = new SecretKeySpec(okmList.get(0),"AES");
+     * </pre>
+     * <p>
+     * This will create a AES key from the <code>okm</code> (output key material).
+     *
+     * @param salt       at least 16 byte long nonce (number used once); salt is not required to be secret
+     * @param ikm        user provided password as byte array or other password reheated entropy
+     * @param costFactor exponential cost (log2 factor) between 4 and 31 e.g. 12 --&gt;
+     *                   2^12 = 4,096 iterations (higher == slower == more secure)
+     * @param configList list of infoParam and outLengths; for every entry this will create an output okm entry
+     * @return list of okm (output key material) with the same size and order as configList
+     */
+    List<byte[]> deriveMulti(byte[] salt, byte[] ikm, int costFactor, List<KdfConfig> configList);
+
+    /**
      * Default implementation
      */
-    class Default implements KeyDerivationFunction {
+    final class Default implements KeyDerivationFunction {
         private static final byte[] FIXED_INFO_PARAM = Bytes.from("bkdf").array();
 
         private final HKDF hkdf;
@@ -88,6 +112,16 @@ public interface KeyDerivationFunction {
 
         @Override
         public byte[] derive(byte[] salt, byte[] ikm, int costFactor, byte[] infoParam, int outLengthByte) {
+            return deriveMulti(salt, ikm, costFactor,
+                    Collections.singletonList(new KdfConfig(infoParam, outLengthByte))).get(0);
+        }
+
+        @Override
+        public List<byte[]> deriveMulti(byte[] salt, byte[] ikm, int costFactor, List<KdfConfig> configList) {
+            if (Objects.requireNonNull(configList).isEmpty()) {
+                throw new IllegalArgumentException("config list must not be empty");
+            }
+
             byte[] extractedPw = hkdf.extract(null, ikm);
 
             BCrypt.HashData data = BCrypt.with(
@@ -96,7 +130,48 @@ public interface KeyDerivationFunction {
                             true, null, null))
                     .hashRaw(costFactor, salt, extractedPw);
 
-            return hkdf.expand(data.rawHash, Bytes.wrapNullSafe(infoParam).append(FIXED_INFO_PARAM).array(), outLengthByte);
+            List<byte[]> outList = new ArrayList<>(configList.size());
+            for (KdfConfig kdfConfig : configList) {
+                outList.add(hkdf.expand(data.rawHash, Bytes.wrapNullSafe(kdfConfig.infoParam).append(FIXED_INFO_PARAM).array(), kdfConfig.outLengthByte));
+            }
+            return outList;
+        }
+    }
+
+    /**
+     * Wraps some of the needed configs for use in KDF
+     */
+    final class KdfConfig {
+        public final byte[] infoParam;
+        public final int outLengthByte;
+
+        /**
+         * Create new instance.
+         *
+         * @param infoParam     optional parameter that can be used to pin the key material to a specific context (e.g.
+         *                      creating a MAC and AES key from same password, just pass <code>"mac".getBytes()</code>
+         *                      and <code>"aes".getBytes()</code> as parameter. Can be null.
+         * @param outLengthByte how many bytes long the resulting key material should be (usually 16 or 32 bytes)
+         */
+        public KdfConfig(byte[] infoParam, int outLengthByte) {
+            this.infoParam = infoParam;
+            this.outLengthByte = outLengthByte;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            KdfConfig kdfConfig = (KdfConfig) o;
+            return outLengthByte == kdfConfig.outLengthByte &&
+                    Arrays.equals(infoParam, kdfConfig.infoParam);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(outLengthByte);
+            result = 31 * result + Arrays.hashCode(infoParam);
+            return result;
         }
     }
 }
--- src/test/java/at/favre/lib/crypto/bkdf/KeyDerivationFunctionTest.java
+++ src/test/java/at/favre/lib/crypto/bkdf/KeyDerivationFunctionTest.java
@@ -3,17 +3,20 @@ package at.favre.lib.crypto.bkdf;
 import at.favre.lib.bytes.Bytes;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class KeyDerivationFunctionTest {
 
     @Test
-    public void testBasicKdf() {
+    public void testBasicKdfPw() {
         KeyDerivationFunction kdf = new KeyDerivationFunction.Default(Version.HKDF_HMAC512);
 
-        char[] pw = "secret".toCharArray();
-        int logRounds = 6;
+        char[] pw = "secret1234 !_".toCharArray();
+        int logRounds = 4;
         byte[] salt = Bytes.random(16).array();
         byte[] secretKey = kdf.derive(salt, pw, logRounds, Bytes.from("aes-key").array(), 32);
 
@@ -21,5 +24,38 @@ public class KeyDerivationFunctionTest {
 
         byte[] secretKey2 = kdf.derive(salt, pw, logRounds, Bytes.from("aes-key").array(), 32);
         assertArrayEquals(secretKey, secretKey2);
+    }
+
+    @Test
+    public void testBasicKdfByteArray() {
+        KeyDerivationFunction kdf = new KeyDerivationFunction.Default(Version.HKDF_HMAC512);
+
+        byte[] pw = Bytes.from("secret1234 !_".toCharArray()).array();
+        int logRounds = 4;
+        byte[] salt = Bytes.random(16).array();
+        byte[] secretKey = kdf.derive(salt, pw, logRounds, Bytes.from("aes-key").array(), 32);
+
+        assertEquals(32, secretKey.length);
+
+        byte[] secretKey2 = kdf.derive(salt, pw, logRounds, Bytes.from("aes-key").array(), 32);
+        assertArrayEquals(secretKey, secretKey2);
+    }
+
+    @Test
+    public void testBasicMultiKdf() {
+        KeyDerivationFunction kdf = new KeyDerivationFunction.Default(Version.HKDF_HMAC512);
+
+        char[] pw = "secret1234 !_".toCharArray();
+        int logRounds = 5;
+        byte[] salt = Bytes.random(16).array();
+
+        List<KeyDerivationFunction.KdfConfig> configList = Arrays.asList(
+                new KeyDerivationFunction.KdfConfig(Bytes.from("aes-key").array(), 16),
+                new KeyDerivationFunction.KdfConfig(Bytes.from("mac-key").array(), 32)
+        );
+        List<byte[]> secretKeyList = kdf.deriveMulti(salt, Bytes.from(pw).array(), logRounds, configList);
+        assertEquals(2, secretKeyList.size());
+        assertEquals(16, secretKeyList.get(0).length);
+        assertEquals(32, secretKeyList.get(1).length);
     }
 }
